5cc4c93ae58a2e950e30f824dc7ebd15e5cabbb0
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-10 01:40:02 -0500

    Update usuarios.js

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -4,5 +4,10 @@ const usuarioCtrl = require('../controllers/usuarioController');
 
 router.post('/register', usuarioCtrl.registrar);
 router.post('/login', usuarioCtrl.login);
+router.get('/ping', (req, res) => {
+  console.log("🔁 Ping recibido");
+  res.send('✅ Backend responde correctamente');
+});
+
 
 module.exports = router;
