c48eeafca004ef9fc7b2ff93248b4c0bc19a260e
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-10 01:49:04 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,9 @@ app.use('/uploads', express.static('uploads'));
 console.log("🕓 A punto de iniciar el servidor...");
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Backend corriendo en el puerto ${PORT}`);
 });
+
 
