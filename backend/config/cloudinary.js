175ae4783644f3ac8c9c03b7cdb64fa15e005db3
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-11 01:33:13 -0500

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,23 +1,19 @@
-// config/cloudinary.js
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'plantas',
-    allowed_formats: ['jpg', 'png', 'jpeg'],
-    public_id: (req, file) => Date.now() + '-' + file.originalname,
-  },
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  }
 });
 
-module.exports = {
-  cloudinary,
-  storage,
-};
+module.exports = { cloudinary, storage };
