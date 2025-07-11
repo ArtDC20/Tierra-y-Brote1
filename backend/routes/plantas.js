416c965889d9574efc48fd44856a128730a2ef50
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-11 01:52:20 -0500

    Update plantas.js

--- backend/routes/plantas.js
+++ backend/routes/plantas.js
@@ -15,12 +15,12 @@ router.get('/', plantaCtrl.obtenerPlantas);
 // Crear planta con imagen en Cloudinary
 router.post('/', verificarToken, verificarAdmin, upload.single('imagen'), (req, res) => {
   const data = req.body;
-  data.imagen_url = req.file ? req.file.path : null; // Cloudinary devuelve `path` completo
+  data.imagen_url = req.file?.path || null; // Cloudinary devuelve `path` completo
   plantaCtrl.crearPlantaConImagen(data, res);
 });
 
 // Actualizar planta
-router.put('/:id', verificarToken, verificarAdmin, upload.single('imagen'), plantaCtrl.actualizarPlanta);
+router.put('/:id', verificarToken, plantaCtrl.actualizarPlanta);
 
 // Eliminar planta
 router.delete('/:id', verificarToken, verificarAdmin, plantaCtrl.eliminarPlanta);
