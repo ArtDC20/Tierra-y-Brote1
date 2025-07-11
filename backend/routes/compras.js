cef0a063f2e3a9a1953eaad8db4dbb151d27defc
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-11 02:40:02 -0500

    Update compras.js

--- backend/routes/compras.js
+++ backend/routes/compras.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const compraCtrl = require('../controllers/compraController');
 const { verificarToken, verificarAdmin } = require('../middleware/auth');
 
-// Ruta para finalizar la compra
+
 router.post('/', compraCtrl.finalizarCompra);
 
-// Nueva ruta para obtener todas las compras (solo admin)
+
 router.get('/', verificarToken, verificarAdmin, compraCtrl.obtenerCompras);
 
 module.exports = router;
