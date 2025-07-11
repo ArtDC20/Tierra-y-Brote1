9c88f40ceaaeb45ec8f45b53afc27116c1f5df38
ArtDC20 <arturoalbuja92@gmail.com>
2025-07-11 02:40:14 -0500

    Update plantas.js

--- backend/routes/plantas.js
+++ backend/routes/plantas.js
@@ -2,27 +2,25 @@ const express = require('express');
 const router = express.Router();
 const plantaCtrl = require('../controllers/plantaController');
 
-// Importar el nuevo middleware desde config/cloudinary.js
+
 const { storage } = require('../config/cloudinary');
 const multer = require('multer');
 const upload = multer({ storage });
 
 const { verificarToken, verificarAdmin } = require('../middleware/auth');
 
-// Obtener todas las plantas
+
 router.get('/', plantaCtrl.obtenerPlantas);
 
-// Crear planta con imagen en Cloudinary
 router.post('/', verificarToken, verificarAdmin, upload.single('imagen'), (req, res) => {
   const data = req.body;
-   data.imagen_url = req.file ? req.file.path : null; // Cloudinary devuelve `path` completo
+   data.imagen_url = req.file ? req.file.path : null; 
   plantaCtrl.crearPlantaConImagen(data, res);
 });
 
-// Actualizar planta
+
 router.put('/:id', verificarToken, plantaCtrl.actualizarPlanta);
 
-// Eliminar planta
 router.delete('/:id', verificarToken, verificarAdmin, plantaCtrl.eliminarPlanta);
 
 module.exports = router;
